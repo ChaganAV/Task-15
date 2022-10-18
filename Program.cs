272ab012c3884726e138d7e2043c7599b7dab6e3
Alexandr <jwchaganav@gmail.com>
2022-10-18 12:01:42 +0400

    end

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿// программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-
-Console.WriteLine("Hello, World!");
+Console.Write("Введите цифру дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day>0 && day <=7)
+{
+    if (day > 5)
+        Console.WriteLine("Да, это выходной");
+    else
+        Console.WriteLine("Нет, это будни");
+}
+else
+    Console.WriteLine("Извините, такого дня недели нет!");
